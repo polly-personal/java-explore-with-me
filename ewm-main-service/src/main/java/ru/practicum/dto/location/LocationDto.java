6f63ce3e58302f9e87a91c474e0a80742ce039f5
PollyPersonal <polly.personal.01@yandex.ru>
2024-01-29 22:52:59 +0300

    2 этап. checkstyle

--- ewm-main-service/src/main/java/ru/practicum/dto/location/LocationDto.java
+++ ewm-main-service/src/main/java/ru/practicum/dto/location/LocationDto.java
@@ -5,14 +5,11 @@ import lombok.Data;
 
 import javax.validation.constraints.DecimalMax;
 import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 
 @Builder
 @Data
 public class LocationDto {
-//    @Max(100000)
-//    @Min(0)
+
     @DecimalMax(value = "90.0000000")
     @DecimalMin(value = "-90.0000000")
     private float lat;
